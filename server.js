7f496a2cab113cb6eef3905988db5315fdb4ac1e
Mahlet Berhane <mahlet.berhane2@gmail.com>
2024-01-27 15:17:14 -0600

    website contents,and  client validation added

--- server.js
+++ server.js
@@ -2,26 +2,33 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // For parsing application/json
-app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 
-// Serve your static files (HTML, CSS, JS, etc.)
-app.use(express.static('public')); // Assuming your static files are in a directory named 'public'
+app.use(express.static('public'));
 
-// Example route for your homepage
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html'); // Adjust the path according to your file structure
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
 app.post('/submit-form', (req, res) => {
-    const { username, email, phoneNumber, suggestion } = req.body;
-    console.log('Username:', username, 'Email:', email, 'PhoneNumer:',phoneNumber,'Suggestion', suggestion);
-    // Here you can handle the form data, e.g., save it to a database, etc.
+    const { name, email, phone , comment } = req.body;
+    console.log('Username:', name, 'Email:', email, 'Tel:', phone,'Comment:', comment);
+    // Handle the form data, save it to a database, etc.
     res.send('Form submitted');
+  });
+
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).send("Sorry, can't find that!");
   });
   
